ddbbc7c00ba05e52c797fb3ea8670c8987e64491
vanshnawander <76697486+vanshnawander@users.noreply.github.com>
2025-06-22 10:50:03 +0530

    meeting link updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -69,7 +69,7 @@ const Hero: React.FC = () => {
               className="btn-primary cursor-pointer"
             >
               <PopupButton
-                url="https://calendly.com/tyloones/mvp-makers"
+                url="https://calendly.com/vanshnawander"
                 /*
                  * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
                  * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
