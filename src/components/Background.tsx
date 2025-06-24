f491e4487cfb7e414d7f154da4bbd4d7c8858269
manshibhatt <bhattmanshi66@gmail.com>
2025-06-24 19:29:16 +0530

    changed bg color

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,7 +1,11 @@
 export default function Background() {
     return (
       <div className="fixed inset-0 -z-10 overflow-visible">
-        <div className="fixed inset-0 -z-10 bg-gradient-to-br from-black via-[#1a001f] to-[#10001a]"></div>
+        {/* <div className="fixed inset-0 -z-10 bg-gradient-to-br from-black via-[#1a001f] to-[#10001a]"></div> */}
+        {/* <div className="fixed inset-0 -z-10 bg-gradient-to-br from-black via-slate-900 to-slate-800"></div> */}
+        <div className="fixed inset-0 -z-10 bg-gradient-to-br from-black via-indigo-950 to-slate-900"></div>
+        {/* <div className="fixed inset-0 -z-10 bg-white" /> */}
+        {/* <div className="fixed inset-0 -z-10 bg-gradient-to-br from-black via-indigo-300 to-indigo-400"></div> */}
         
       </div>
     );
--- src/components/ui/SectionHeading.tsx
+++ src/components/ui/SectionHeading.tsx
@@ -32,7 +32,7 @@ const SectionHeading: React.FC<SectionHeadingProps> = ({
       {subtitle && (
         <motion.p 
           variants={fadeInUp}
-          className="text-slate-600 dark:text-slate-400 max-w-3xl mx-auto"
+          className="dark:white max-w-3xl mx-auto"
         >
           {subtitle}
         </motion.p>
